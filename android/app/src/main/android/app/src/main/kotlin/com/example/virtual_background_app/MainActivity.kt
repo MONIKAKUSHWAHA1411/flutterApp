fcff8287034f32b4280f4c1e17db0a17af6dfc24
Monika Kushwaha <62650446+MONIKAKUSHWAHA1411@users.noreply.github.com>
2025-06-20 01:11:34 +0530

    Update MainActivity.kt

--- android/app/src/main/android/app/src/main/kotlin/com/example/virtual_background_app/MainActivity.kt
+++ android/app/src/main/android/app/src/main/kotlin/com/example/virtual_background_app/MainActivity.kt
@@ -2,6 +2,4 @@ package com.example.virtual_background_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
-
+class MainActivity: FlutterActivity() {}
